1ecb25a61cdc75ef7fe4ec14ebb2616f13a3184c
praveensunar <140327889+praveensunar@users.noreply.github.com>
2026-03-04 22:55:35 +0530

    feat: Implement `NoData` component, add various image assets, and configure the project's `.gitignore`.

--- Frontend/src/Components/NoData.jsx
+++ Frontend/src/Components/NoData.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import noDataImage from '../assets/nothing here yet.webp'
+import noDataImage from '../assets/nothing_here_yet.webp'
 
 const NoData = () => {
   return (
     <div className='flex flex-col items-center justify-center p-4 gap-2'>
-        <img src={noDataImage} alt="no Data" className='w-36'/>
-        <p className='text-neutral-500'>No Data</p>
+      <img src={noDataImage} alt="no Data" className='w-36' />
+      <p className='text-neutral-500'>No Data</p>
     </div>
   )
 }
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import banner from '../assets/banner.jpg'
-import bannerMobile from '../assets/banner-Mobile.jpg'
+import bannerMobile from '../assets/banner-mobile.jpg'
 import { useSelector } from 'react-redux'
 import { valideURLConvert } from '../utils/valideURLConvert'
 import { Link, useNavigate } from 'react-router-dom'
@@ -8,75 +8,75 @@ import CategoryWiseProductDisplay from '../Components/CategoryWiseProductDisplay
 
 const Home = () => {
   const loadingCategory = useSelector(state => state.product.loadingCategory)
-  const categoryData = useSelector(state =>state.product.allCategory)
-  const subCategoryData = useSelector(state =>state.product.allSubCategory)
+  const categoryData = useSelector(state => state.product.allCategory)
+  const subCategoryData = useSelector(state => state.product.allSubCategory)
   const navigate = useNavigate()
 
-  const handleRedirectProductList =(id,cat)=>{
-    console.log("category:",id,cat)
+  const handleRedirectProductList = (id, cat) => {
+    console.log("category:", id, cat)
 
-    const subcategory = subCategoryData.find(sub =>{
-      const filterData = sub.category.some(c =>{
+    const subcategory = subCategoryData.find(sub => {
+      const filterData = sub.category.some(c => {
         return c._id == id
       })
       return filterData ? true : null
 
     })
-      const url = `/${valideURLConvert(cat)}-${id}/${valideURLConvert(subcategory.name)}-${subcategory._id}`
-      navigate(url)
+    const url = `/${valideURLConvert(cat)}-${id}/${valideURLConvert(subcategory.name)}-${subcategory._id}`
+    navigate(url)
 
   }
   return (
 
     <section className='bg-white'>
-            <div className='container mx-auto'>
-                <div className={`w-full h-full  bg-blue-50 min-h-48 rounded ${!banner && "animate-pulse"}`}>
-                    <img src={banner} alt="Banner" className='w-full h-full hidden lg:block' />
+      <div className='container mx-auto'>
+        <div className={`w-full h-full  bg-blue-50 min-h-48 rounded ${!banner && "animate-pulse"}`}>
+          <img src={banner} alt="Banner" className='w-full h-full hidden lg:block' />
 
-                    <img src={bannerMobile} alt="Banner" className='w-full h-full lg:hidden' />
+          <img src={bannerMobile} alt="Banner" className='w-full h-full lg:hidden' />
 
-                </div>
+        </div>
 
-                <div className='container mx-auto grid grid-cols-5 lg:grid-cols-10 md:grid-cols-8 gap-2'>
+        <div className='container mx-auto grid grid-cols-5 lg:grid-cols-10 md:grid-cols-8 gap-2'>
 
-                  {
-                    loadingCategory ?(
-                    new Array(20).fill(null).map((c,index)=>{
-                        return (
-                          <div key={index+"loadingCagtegory"} className='bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse'>
-                            <div className='bg-blue-100 min-h-20 rounded'></div>
-                            <div className='bg-blue-100 h-8 rounded'></div>                           
-                          </div>
-                        )
-                    })
-                  ):
-                  (
-                    categoryData.map((cat,index)=>{
-                      return(
-                    <div key={cat._id+"displayCategory"} className='w-full h-full' onClick={()=>handleRedirectProductList(cat._id,cat.name)}>
+          {
+            loadingCategory ? (
+              new Array(20).fill(null).map((c, index) => {
+                return (
+                  <div key={index + "loadingCagtegory"} className='bg-white rounded p-4 min-h-36 grid gap-2 shadow animate-pulse'>
+                    <div className='bg-blue-100 min-h-20 rounded'></div>
+                    <div className='bg-blue-100 h-8 rounded'></div>
+                  </div>
+                )
+              })
+            ) :
+              (
+                categoryData.map((cat, index) => {
+                  return (
+                    <div key={cat._id + "displayCategory"} className='w-full h-full' onClick={() => handleRedirectProductList(cat._id, cat.name)}>
                       <div>
                         <img src={cat.image}
-                        className='w-full object-scale-down'
+                          className='w-full object-scale-down'
                         />
                       </div>
                     </div>
-                      )
-                    })
-                    
                   )
-                  }
-                </div>
-            </div>
-                  {/* display category product */}
-                  {
-                    categoryData.map((c,index)=>{
-                      return(
-                        <CategoryWiseProductDisplay key={c?._id+"categorywishProduct"} id={c?._id} name={c?.name}/>
+                })
 
-                      )
-                    })
-                  }
-            
+              )
+          }
+        </div>
+      </div>
+      {/* display category product */}
+      {
+        categoryData.map((c, index) => {
+          return (
+            <CategoryWiseProductDisplay key={c?._id + "categorywishProduct"} id={c?._id} name={c?.name} />
+
+          )
+        })
+      }
+
 
     </section>
   )
