930b89c94c2bcbdbbf985960a8293e42dceddd1c
praveensunar <140327889+praveensunar@users.noreply.github.com>
2026-03-04 22:59:04 +0530

    updated

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.png'
-import Search from '../Components/search'
+import Search from './Search'
 import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom'
 import { FaRegCircleUser } from "react-icons/fa6";
 import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
@@ -18,33 +18,33 @@ const Header = () => {
     const [isMobile] = useMobile()
     const location = useLocation()
     const isSearchPage = location.pathname === "/search"
-    const navigate =useNavigate()
-    const user = useSelector((state)=> state?.user)
-    const [openUserMenu,setOpenUserMenu] = useState(false)
-    const cartItem =useSelector(state => state.cartItem.cart)
+    const navigate = useNavigate()
+    const user = useSelector((state) => state?.user)
+    const [openUserMenu, setOpenUserMenu] = useState(false)
+    const cartItem = useSelector(state => state.cartItem.cart)
     // const [ totalQty ,setTotalQty ] = useState(0)
     // const [ totalPrice,setTotalPrice ] = useState(0)
-    const {totalQty ,totalPrice} =useGlobalContext()
-    const [openCartSection , setOpenCartSection ]= useState(false)
+    const { totalQty, totalPrice } = useGlobalContext()
+    const [openCartSection, setOpenCartSection] = useState(false)
 
-    const redirectToLoginPage = ()=>{
+    const redirectToLoginPage = () => {
         navigate("/login")
     }
 
-    const handleCloseUserMenu = ()=>{
+    const handleCloseUserMenu = () => {
         setOpenUserMenu(false)
     }
 
-    const handleMobileUser = ()=>{
-            if(!user._id){
-                navigate("/login")
-                return
-            }
+    const handleMobileUser = () => {
+        if (!user._id) {
+            navigate("/login")
+            return
+        }
 
-            navigate('/user')
+        navigate('/user')
     }
-    
-    
+
+
     //total price and total price
 
     // useEffect(()=>{
@@ -52,110 +52,110 @@ const Header = () => {
     //         return preve + curr.quntity
     //     },0)
     //     setTotalQty(qty)
-        
+
     //     const tPrice = cartItem.reduce((preve,curr)=>{
     //         return preve + (curr.productId.price * curr.quntity)
     //     },0)
     //     setTotalPrice(tPrice)
 
-        
+
     // },[cartItem])
-  return (
-    <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
-        {
-            !(isSearchPage && isMobile) && (
-                <div className='container mx-auto flex items-center px-2 justify-between'>
-  {/* logo */}
-    <div className='h-full'>
-        <Link to={"/"} className='h-full flex justify-center items-center'>
-            <img src={logo} alt="logo" width={170} height={60} className='hidden lg:block' />
-                {/*Mobile view */}
-            <img src={logo} alt="logo" width={120} height={60} className='lg:hidden '  />
-        </Link>
-    </div>
-        {/*search */}
-        <div className='hidden lg:block'>
-            <Search/>
-        </div>
+    return (
+        <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
+            {
+                !(isSearchPage && isMobile) && (
+                    <div className='container mx-auto flex items-center px-2 justify-between'>
+                        {/* logo */}
+                        <div className='h-full'>
+                            <Link to={"/"} className='h-full flex justify-center items-center'>
+                                <img src={logo} alt="logo" width={170} height={60} className='hidden lg:block' />
+                                {/*Mobile view */}
+                                <img src={logo} alt="logo" width={120} height={60} className='lg:hidden ' />
+                            </Link>
+                        </div>
+                        {/*search */}
+                        <div className='hidden lg:block'>
+                            <Search />
+                        </div>
 
-        {/**login and my cart */}
-        <div className=''>
-            {/* user icons displays in only mobile version */}
-            <button className='text-neutral-600 lg:hidden' onClick={handleMobileUser}>
-                <FaRegCircleUser size={26} />
-            </button>
+                        {/**login and my cart */}
+                        <div className=''>
+                            {/* user icons displays in only mobile version */}
+                            <button className='text-neutral-600 lg:hidden' onClick={handleMobileUser}>
+                                <FaRegCircleUser size={26} />
+                            </button>
 
-            {/* Desktop */}
+                            {/* Desktop */}
 
-            <div className='hidden lg:flex items-center gap-10'>
-                {
-                    user?._id ?(
-                        <div className='relative'>
-                                <div  onClick={()=>setOpenUserMenu(preve => !preve)} className='flex select-none items-center gap-1 cursor-pointer'>
-                                <p>Account</p>
+                            <div className='hidden lg:flex items-center gap-10'>
                                 {
-                                    openUserMenu ?(
-                                        <GoTriangleUp size={25} />
-                                    ):(
-                                        <GoTriangleDown size={25} />
+                                    user?._id ? (
+                                        <div className='relative'>
+                                            <div onClick={() => setOpenUserMenu(preve => !preve)} className='flex select-none items-center gap-1 cursor-pointer'>
+                                                <p>Account</p>
+                                                {
+                                                    openUserMenu ? (
+                                                        <GoTriangleUp size={25} />
+                                                    ) : (
+                                                        <GoTriangleDown size={25} />
+                                                    )
+                                                }
+                                            </div>
+                                            {
+                                                openUserMenu && (
+                                                    <div className='absolute right-0 top-12'>
+                                                        <div className='bg-white rounded p-4 min-w-52 lg:shadow-lg'>
+                                                            <UserMenu close={handleCloseUserMenu} />
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+
+
+
+
+                                        </div>
+                                    ) : (
+                                        <button onClick={redirectToLoginPage} className='text-lg px-2'>Login</button>
                                     )
                                 }
-                                 </div>
-                                 {
-                                    openUserMenu && (
-                                        <div className='absolute right-0 top-12'>
-                                    <div className='bg-white rounded p-4 min-w-52 lg:shadow-lg'>
-                                        <UserMenu close={handleCloseUserMenu}/>
-                                        </div>
-                                        </div>
-                                    )
-                                 }
-                                
-                                
-                               
 
-                        </div>
-                    ) : (
-                    <button onClick={redirectToLoginPage} className='text-lg px-2'>Login</button>
-                    )
-                }
-                
-                    <button onClick={()=>setOpenCartSection(true)} className='flex items-center gap-2 bg-green-700 hover:bg-green-800 p-3 rounded text-white'>
-                        {/* add to cart */}
-                        <div className='animate-bounce'>
-                        <BsCart4 size={25}/>
-                        </div>
-                        <div className='font-semibold text-sm'>
-                            {
-                                cartItem[0] ?(
-                                    <div>
-                                        <p>{totalQty} items</p>
-                                        <p>{DisplayPriceInRupees(totalPrice)}</p>
+                                <button onClick={() => setOpenCartSection(true)} className='flex items-center gap-2 bg-green-700 hover:bg-green-800 p-3 rounded text-white'>
+                                    {/* add to cart */}
+                                    <div className='animate-bounce'>
+                                        <BsCart4 size={25} />
                                     </div>
-                                ):(
-                                    <p>My Cart</p>
-                                )
-                            }
-                            
+                                    <div className='font-semibold text-sm'>
+                                        {
+                                            cartItem[0] ? (
+                                                <div>
+                                                    <p>{totalQty} items</p>
+                                                    <p>{DisplayPriceInRupees(totalPrice)}</p>
+                                                </div>
+                                            ) : (
+                                                <p>My Cart</p>
+                                            )
+                                        }
+
+                                    </div>
+                                </button>
+                            </div>
                         </div>
-                    </button>    
+                    </div>
+                )
+            }
+
+            <div className='container mx-auto px-2 lg:hidden'>
+                <Search />
             </div>
-        </div>
-        </div>
-            )
-        }
-        
-        <div className='container mx-auto px-2 lg:hidden'>
-            <Search/>
-        </div>
-        {
-            openCartSection && (
-                <DisplayCartItem close={()=>setOpenCartSection(false)}/>
-            )
-        }
-      
-    </header>
-  )
+            {
+                openCartSection && (
+                    <DisplayCartItem close={() => setOpenCartSection(false)} />
+                )
+            }
+
+        </header>
+    )
 }
 
 export default Header
